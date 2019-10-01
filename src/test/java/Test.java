6225da218f9279d0d4bb6a4b43b4627a9fbf7aa1
liuu2811751 <liudeng_17@163.com>
2019-10-01 17:11:43 +0800

    dd

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -7,7 +7,7 @@ public class Test {
         GoogleApi googleApi = new GoogleApi();
         // 通过代理
 //        GoogleApi googleApi = new GoogleApi("122.224.227.202", 3128);
-        String result = googleApi.translate("hello world",  "zh");
+        String result = googleApi.translate("这个 还有货吗", "zh-CN", "id");
         System.out.println(result);
     }
 }
