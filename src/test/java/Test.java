2cf34937fda4f32cc49a265c8ddb2f23d6477fee
liuu2811751 <liudeng_17@163.com>
2019-11-22 19:28:06 +0800

    dd

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -7,7 +7,7 @@ public class Test {
         GoogleApi googleApi = new GoogleApi();
         // 通过代理
 //        GoogleApi googleApi = new GoogleApi("122.224.227.202", 3128);
-        String result = googleApi.translate("你个大傻比", "zh-CN", "id");
+        String result = googleApi.translate("刚刚，64人新当选中科院院士！", "zh-CN", "id");
         System.out.println(result);
     }
 }
